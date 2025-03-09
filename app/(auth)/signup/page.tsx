22e7de3953ac02a2c0a37f3caec89fb6cb894c4e
Pratham Patel <prathampatel45201@gmail.com>
2025-03-10 02:08:20 +0530

    Completed signup and signin mutations in graphql

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { Button, Input } from '@nextui-org/react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useMutation } from 'urql'
+import { SignupMutation } from '@/gql/signupMutation'
+import { setToken } from '@/utils/token'
+
+const SignupPage = () => {
+  const [state, setState] = useState({ password: '', email: '' })
+  const router = useRouter()
+  const [singupResult, signup] = useMutation(SignupMutation)
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    const response = await signup({ input: state })
+    if (response?.data?.createUser) {
+      setToken(response?.data?.createUser?.token)
+      router.push('/')
+    }
+  }
+
+  return (
+    <div className="bg-white rounded-md border p-4 w-full shadow-sm">
+      <div className="text-2xl text-black/70">Sign up</div>
+      <form onSubmit={handleSignup} className="flex flex-col gap-4 mt-4">
+        <div>
+          <Input
+            value={state.email}
+            onValueChange={(v) => setState((s) => ({ ...s, email: v }))}
+            variant="faded"
+            label="Email"
+            classNames={{
+              inputWrapper: 'bg-slate-50 border-slate-100',
+            }}
+          />
+        </div>
+        <div>
+          <Input
+            variant="faded"
+            value={state.password}
+            onValueChange={(v) => setState((s) => ({ ...s, password: v }))}
+            label="Password"
+            type="password"
+            classNames={{ inputWrapper: 'bg-slate-50 border-slate-100' }}
+          />
+        </div>
+        <div className="text-end">
+          <Button type="submit" variant="solid" color="primary">
+            Signup
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignupPage
